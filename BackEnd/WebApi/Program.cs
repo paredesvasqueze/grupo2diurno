7f4fcf2b36f716173e41a0e0f79c99c94bd75bde
Augusto Pascual <pascualzevallosc@iestptrujillo.net>
2024-10-31 12:06:35 -0500

    cambios

--- BackEnd/WebApi/Program.cs
+++ BackEnd/WebApi/Program.cs
@@ -76,6 +76,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
